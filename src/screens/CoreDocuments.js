606c96eebe74883dcbc945a93d7fe486db750564
Rukosuev Nikita <aletagro@yandex.ru>
2024-10-23 17:13:44 +0500

    new data_version: 181

--- src/screens/CoreDocuments.js
+++ src/screens/CoreDocuments.js
@@ -8,7 +8,9 @@ const coreDocumentsId = 'e918110c-418e-4a50-90bc-484581a0fa5c'
 const rulesImage = 'https://dhss9aar8ocw.cloudfront.net/09a5c1fd-a186-4517-8a6f-0a107374eb45'
 
 const CoreDocuments = () => {
-    const documents = dataBase.data.publication.filter((publication) => publication.publicationGroupId === coreDocumentsId && !publication.spearheadName)
+    const documentsIds = dataBase.data.publication_publication_group.filter((publication) => publication.publicationGroupId === coreDocumentsId)
+    let documents = documentsIds.map(({publicationId}) => dataBase.data.publication.find(publication => publication.id === publicationId))
+    documents = documents.filter(document => !document.spearheadName)
     documents.sort((a, b) => a.displayOrder - b.displayOrder)
 
     const renderRow = (document) => <Row
