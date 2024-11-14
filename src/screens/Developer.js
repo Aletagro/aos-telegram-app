191d582725284f993d20698a069bca93390e23ac
Rukosuev Nikita <aletagro@yandex.ru>
2024-11-15 00:05:02 +0500

    test

--- src/screens/Developer.js
+++ src/screens/Developer.js
@@ -3,12 +3,21 @@ import React from 'react';
 const tg = window.Telegram.WebApp
 
 const Developer = () => {
+
+    const handleAddInStorage = () => {
+        tg.CloudStorage?.setItem('key', 'value')
+    }
+
     return <div>
         <p>test</p>
         <p>initData: {tg.initData}</p>
+        <p>initDataUnsafe: {JSON.stringify(tg.initDataUnsafe)}</p>
         <p>platform: {tg.platform}</p>
         <p>version: {tg.version}</p>
         <p>viewportHeight: {tg.viewportHeight}</p>
+        <p>CloudStorageKeys: {JSON.stringify(tg.CloudStorage?.getKeys())}</p>
+        <p>CloudStorageValue: {JSON.stringify(tg.CloudStorage?.getItems())}</p>
+        <button onClick={handleAddInStorage}>setItemInCloudStorage</button>
     </div>
 }
 
